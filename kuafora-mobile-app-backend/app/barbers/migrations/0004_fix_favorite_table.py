80c5a99aad65f87bb1431ab843cf7499882acd7c
Talha Colfaoğlu <colfaoglutalha@gmail.com>
2025-09-04 17:43:26 +0300

    New migration added app/barbers/migrations/0004_fix_favorite_table.py

---
+++ kuafora-mobile-app-backend/app/barbers/migrations/0004_fix_favorite_table.py
@@ -0,0 +1,29 @@
+from django.db import migrations
+
+
+SQL_CREATE_FAVORITE = r'''
+CREATE TABLE IF NOT EXISTS barbers_favorite (
+    id BIGSERIAL PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users_user(id) DEFERRABLE INITIALLY DEFERRED,
+    barbershop_id BIGINT NOT NULL REFERENCES barbers_barbershop(id) DEFERRABLE INITIALLY DEFERRED,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+CREATE UNIQUE INDEX IF NOT EXISTS barbers_favorite_user_barbershop_uniq
+    ON barbers_favorite (user_id, barbershop_id);
+CREATE INDEX IF NOT EXISTS barbers_fav_user_id_3bb9b3_idx
+    ON barbers_favorite (user_id, created_at DESC);
+'''
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('barbers', '0003_lastviewed'),
+        ('users', '0003_alter_lastviewed_unique_together_and_more'),
+    ]
+
+    operations = [
+        migrations.RunSQL(SQL_CREATE_FAVORITE, migrations.RunSQL.noop),
+    ]
+
+
