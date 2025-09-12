c767f42b366398fe7cd4347d68b463577c2f590f
Talha Colfaoğlu <colfaoglutalha@gmail.com>
2025-09-12 17:58:44 +0300

    A fix in models.py about defaul value of created_at parameter

--- kuafora-mobile-app-backend/app/barbers/models.py
+++ kuafora-mobile-app-backend/app/barbers/models.py
@@ -295,7 +295,7 @@ class Service(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration = models.PositiveIntegerField(help_text="Duration in minutes")
     is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
 
     def __str__(self) -> str:
         return f"{self.barbershop.name} - {self.name}"
