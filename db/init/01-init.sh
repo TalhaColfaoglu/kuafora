034eba44ee5d619d8c2b1a7b977d4781aea5e2fd
Talha Colfaoğlu <colfaoglutalha@gmail.com>
2025-11-04 20:43:04 +0300

    fixes

--- db/init/01-init.sh
+++ db/init/01-init.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 set -e
 
+# Create roles if not exists (allowed inside DO), and ensure passwords
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  DO \$\$
-  BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$BACKEND_USER') THEN
-      EXECUTE format('CREATE USER %I WITH PASSWORD %L;', '$BACKEND_USER', '$BACKEND_PASSWORD');
-    END IF;
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '$BACKEND_DB') THEN
-      EXECUTE format('CREATE DATABASE %I OWNER %I;', '$BACKEND_DB', '$BACKEND_USER');
-    END IF;
+DO \$\$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$BACKEND_USER') THEN
+    EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L;', '$BACKEND_USER', '$BACKEND_PASSWORD');
+  END IF;
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$WEBSITE_USER') THEN
+    EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L;', '$WEBSITE_USER', '$WEBSITE_PASSWORD');
+  END IF;
+END
+\$\$;
 
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$WEBSITE_USER') THEN
-      EXECUTE format('CREATE USER %I WITH PASSWORD %L;', '$WEBSITE_USER', '$WEBSITE_PASSWORD');
-    END IF;
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '$WEBSITE_DB') THEN
-      EXECUTE format('CREATE DATABASE %I OWNER %I;', '$WEBSITE_DB', '$WEBSITE_USER');
-    END IF;
-  END
-  \$\$;
+ALTER ROLE "$BACKEND_USER" WITH PASSWORD '$BACKEND_PASSWORD';
+ALTER ROLE "$WEBSITE_USER" WITH PASSWORD '$WEBSITE_PASSWORD';
 EOSQL
+
+# Create databases if not exists (cannot run inside DO)
+if ! psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname = '$BACKEND_DB'" | grep -q 1; then
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE \"$BACKEND_DB\" OWNER \"$BACKEND_USER\";"
+fi
+
+if ! psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname = '$WEBSITE_DB'" | grep -q 1; then
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE \"$WEBSITE_DB\" OWNER \"$WEBSITE_USER\";"
+fi
 
 
