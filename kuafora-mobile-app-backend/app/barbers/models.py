09c9de46fbb62b34518de73e52a93ce1df4406bb
Talha Colfaoğlu <colfaoglutalha@gmail.com>
2025-12-02 21:08:49 +0300

    ...

--- kuafora-mobile-app-backend/app/barbers/models.py
+++ kuafora-mobile-app-backend/app/barbers/models.py
@@ -425,7 +425,7 @@ class StaffService(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         unique_together = ("staff", "service")
         ordering = ["service__name"]
